3160190a2df648c4ea24d6484e21afbdcc9a41bc
Harri Eronen <harri.eronen@compile.fi>
2018-05-09 07:38:31 +0300

    Make PathStack optionally create filesystempaths

--- yti-taxgen-ycl-source-parser/src/main/kotlin/fi/vm/yti/taxgen/yclsourceparser/sourcebundle/folderwriter/PathStack.kt
+++ yti-taxgen-ycl-source-parser/src/main/kotlin/fi/vm/yti/taxgen/yclsourceparser/sourcebundle/folderwriter/PathStack.kt
@@ -3,25 +3,44 @@ package fi.vm.yti.taxgen.yclsourceparser.sourcebundle.folderwriter
 import java.nio.file.Path
 import java.util.LinkedList
 
-class PathStack(private val rootPath: Path) {
+class PathStack(
+    private val rootPath: Path,
+    private val createFileSystemPaths: Boolean = false
+) {
 
     private val elementStack = LinkedList<String>()
+    private var pathCache: Path? = null
 
     fun pushSubfolderWithIndex(subfolderName: String, index: Int) {
         elementStack.push("${subfolderName}_$index")
+        resetCache()
+        createFileSystemPath()
     }
 
     fun pop() {
         elementStack.pop()
+        resetCache()
     }
 
     fun currentPath(): Path {
-        return elementStack.foldRight(rootPath) { element, path ->
-            path.resolve(element)
-        }
+        return pathCache ?: buildCurrentPath()
     }
 
     fun resolvePath(filename: String): Path {
         return currentPath().resolve(filename)
+    }
+
+    private fun buildCurrentPath(): Path {
+        return elementStack
+            .foldRight(rootPath) { element, path -> path.resolve(element) }
+            .also { pathCache = it }
+    }
+
+    private fun resetCache() {
+        pathCache = null
+    }
+
+    private fun createFileSystemPath() {
+        if (createFileSystemPaths) currentPath().toFile().mkdirs()
     }
 }
